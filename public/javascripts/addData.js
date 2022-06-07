489d34bfc3b73351fc6dfc1864f15d3822d804fb
CharlesLee-23 <Coleman.hallom@Spray.com>
2022-06-07 12:53:46 -0500

    changed script

---
+++ public/javascripts/addData.js
@@ -0,0 +1,32 @@
+var body = document.getElementsByTagName("body")[0];
+
+const xhr = new XMLHttpRequest(),
+	method = "GET",
+	url = "https://testing-api-one.herokuapp.com/";
+
+	function addInfo(){
+		
+xhr.open(method, url, true);
+if(!('withCredentials' in xhr)){
+	alert('No CORS');
+}
+else{
+xhr.onreadystatechange = function () {
+	if (this.readyState == 4 && this.status == 200) {
+		var newArr = JSON.parse(this.responseText);
+		if(newArr.stat == 'ok'){
+		for (var i = 0; i < newArr.length; i++) {
+			let divs = document.createElement("div");
+			let texts = document.createTextNode(newArr[i].name);
+			divs.appendChild(texts);
+			body.appendChild(divs);
+		}
+	}
+	}
+
+};
+xhr.send();
+
+
+}
+}
