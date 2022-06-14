7d137fd437edbcac8eeb56a74c4ed8b31d2cb968
Coleman <charleebrown22@gmail.com>
2022-06-14 11:57:50 -0500

    Changed script names

---
+++ public/javascripts/putData.js
@@ -0,0 +1,32 @@
+
+
+const xhr = new XMLHttpRequest(),
+method = "POST",
+url = "https://testing-api-one.herokuapp.com/";
+
+function addInfo(){
+var body = document.getElementsByTagName("body")[0];
+xhr.open(method, url, true);
+if(!('withCredentials' in xhr)){
+alert('No CORS');
+}
+else{
+xhr.onreadystatechange = function () {
+if (this.readyState == 4 && this.status == 200) {
+	var newArr = JSON.parse(this.responseText);
+
+	for (var i = 0; i < newArr.length; i++) {
+		let divs = document.createElement("div");
+		let texts = document.createTextNode(newArr[i].name);
+		divs.appendChild(texts);
+		body.appendChild(divs);
+	}
+
+}
+
+};
+xhr.send();
+
+
+}
+}
